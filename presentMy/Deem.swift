6486aced02544a5bee608443e591a0402d774be3
Deem Ibrahim Alfaleh <dfalfaleh@gmail.com>
2024-09-29 16:09:45 +0300

    add deem page

---
+++ presentMy/Deem.swift
@@ -0,0 +1,18 @@
+//
+//  Deem.swift
+//  presentMy
+//
+//  Created by Deem Ibrahim on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct Deem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Deem()
+}
