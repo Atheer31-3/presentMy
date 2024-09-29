7d070eee34cfaa1998f606d8667ae517e948474d
khuloud00 <khuloud.turkay@gmail.com>
2024-09-29 15:19:28 +0300

    added khuod page

---
+++ khuloud.swift
@@ -0,0 +1,18 @@
+//
+//  khuloud.swift
+//  presentMy
+//
+//  Created by khuloud nasser on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct khuloud: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    khuloud()
+}
