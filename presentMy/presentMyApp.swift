3a91ee02d154789fa09efbbf490b0a13449f443e
Atheer Alshareef <105993810+Atheer31-3@users.noreply.github.com>
2024-09-29 15:00:18 +0300

    Initial Commit

---
+++ presentMy/presentMyApp.swift
@@ -0,0 +1,17 @@
+//
+//  presentMyApp.swift
+//  presentMy
+//
+//  Created by atheer alshareef on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct presentMyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
