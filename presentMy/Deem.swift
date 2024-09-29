3eb1bb7b552cba53dade64b8d2beb9df72a36906
Deem Ibrahim Alfaleh <dfalfaleh@gmail.com>
2024-09-29 16:46:49 +0300

    edit

--- presentMy/Deem.swift
+++ presentMy/Deem.swift
@@ -9,10 +9,22 @@ import SwiftUI
 
 struct Deem: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Image("house") .resizable()  .clipShape(Circle()).scaledToFit() .frame(width: 300, height: 300)
+            Text("Deem Alfaleh")
+                .font(.system(size: 24, weight: .bold))
+                .foregroundColor(.brown)
+                    
+                   
+            Text("\n I love autumn and programming. The beauty of fall inspires me, and I enjoy coding to create and solve challenges. I’m in my final year of university, ready to take on new adventures.")
+        }
+        
+
     }
+    
 }
 
 #Preview {
     Deem()
 }
+
