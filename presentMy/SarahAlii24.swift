e0b44a7604b4b26df79c9f4fbb8216630984e694
SarahAlii24 <saraaliialahmadi@gmail.com>
2024-09-29 15:15:59 +0300

    added sara page

---
+++ presentMy/SarahAlii24.swift
@@ -0,0 +1,18 @@
+//
+//  SarahAlii24.swift
+//  presentMy
+//
+//  Created by Sara Ali Alahmadi on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct SarahAlii24: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SarahAlii24()
+}
