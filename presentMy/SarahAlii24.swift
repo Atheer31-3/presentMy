564d41876ecb08ecb3fab1a895de1c71ee0868f1
SarahAlii24 <saraaliialahmadi@gmail.com>
2024-09-29 17:17:21 +0300

    finsh page sara

--- presentMy/SarahAlii24.swift
+++ presentMy/SarahAlii24.swift
@@ -12,9 +12,22 @@ struct SarahAlii24: View {
         VStack{
     Image("1")
     .resizable()
-    .frame(width: 200.0, height: 200.0)
-    .cornerRadius(40)
+    .frame(width: 250, height: 250)
+    .cornerRadius(100)
+    .clipShape(Circle())
+    .overlay(Circle().stroke(Color.yellow, lineWidth: 7))
+    .padding()
+            Text("Sarah Alahmadi")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(Color.red)
+                .padding(.leading, -160.0)
+                .padding(.bottom,1)
+            Text("I'm an Artist, I love Design my intrest is to merge our culture in designes. I'm intrested in many topics and i have experiance in, such as Education, Fashion, Crafts and Tecnical especially Front-End Developing and UI/UX Design my purpos is to use all my background to create creative Apps and solving proplems.")
+                .font(.footnote)
+                
         }
+        .padding(.horizontal)
     }
 }
 
