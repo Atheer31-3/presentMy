fccbf422087fd48750123e1f7d9fea6b0d6186e5
Atheer Alshareef <105993810+Atheer31-3@users.noreply.github.com>
2024-09-29 15:10:38 +0300

    add atheer

---
+++ presentMy/Atheer.swift
@@ -0,0 +1,18 @@
+//
+//  Atheer.swift
+//  presentMy
+//
+//  Created by atheer alshareef on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct Atheer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Atheer()
+}
